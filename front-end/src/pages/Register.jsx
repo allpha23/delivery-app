4c2bb8db2ef9b22c029f6eff1e5f3bd0fc365a25
Rafael Bonin <rafalyf11@gmail.com>
2022-08-04 17:48:56 -0300

    feat: implementacao de requisitos para habilitar o botao de registro

--- front-end/src/pages/Register.jsx
+++ front-end/src/pages/Register.jsx
@@ -1,21 +1,47 @@
 import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
 
 function Register() {
+  const history = useHistory();
+
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
+  const minPasswordLength = 6;
+  const minNameLength = 12;
+  const emailRegex = /\S+@\S+\.\S+/;
+
+  const btnStatus = !(name.length >= minNameLength
+    && emailRegex.test(email) && (password.length >= minPasswordLength));
+
   return (
     <>
-      <input type='text' data-testid='common_register__input-name' placeholder='Seu Nome' />
-      <input type='text' data-testid='common_register__input-email' placeholder='seu-email@site.com.br' />
-      <input type='password' data-testid='common_register__input-password' placeholder='********' />
-      <button type='button' data-testid='common_register__button-register' onClick={ () => (name, email, password) }>
+      <input
+        type="text"
+        data-testid="common_register__input-name"
+        placeholder="Seu Nome"
+      />
+      <input
+        type="text"
+        data-testid="common_register__input-email"
+        placeholder="seu-email@site.com.br"
+      />
+      <input
+        type="password"
+        data-testid="common_register__input-password"
+        placeholder="********"
+      />
+      <button
+        type="button"
+        data-testid="common_register__button-register"
+        disabled={ btnStatus }
+        onClick={ () => (name) }
+      >
         CADASTRAR
       </button>
     </>
   );
-
 }
 
 export default Register;
