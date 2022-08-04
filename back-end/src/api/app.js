fd8dc3f2542ee28fabec677401f3d9d1425dbb80
brucealberto <brucealberto2@gmail.com>
2022-08-04 18:20:26 -0300

    feat: login to app

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,7 +1,11 @@
-const express = require('express');
+const express = require("express");
+const loginRoute = require("../routes/loginRoute");
 
 const app = express();
+app.use(express.json());
+// app.use("/login", loginRoute);
+post("/login", loginRoute );
 
-app.get('/coffee', (_req, res) => res.status(418).end());
+app.get("/coffee", (_req, res) => res.status(418).end());
 
 module.exports = app;
