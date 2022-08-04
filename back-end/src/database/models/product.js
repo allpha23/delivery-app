4885b82a7392e6a88ab6bbd41ea66eabe4b327c2
brucealberto <brucealberto2@gmail.com>
2022-08-04 16:58:12 -0300

    feat: adiciona migração e entidade product

---
+++ back-end/src/database/models/product.js
@@ -0,0 +1,29 @@
+"use strict";
+
+module.exports = (sequelize, DataTypes) => {
+  const Product = sequelize.define(
+    "product",
+    {
+      id: {
+        primaryKey: true,
+        type: DataTypes.STRING,
+        autoIncrement: true,
+      },
+      name: DataTypes.STRING,
+      price: DataTypes.DECIMAL(4,2),
+      url_image: DataTypes.STRING,
+    },
+    {
+      timestamps: false,
+    }
+  );
+
+  // Product.associate = (models) => {
+  //   Product.hasMany(models.sales, {
+  //     foreignKey: "user_id",
+  //     as: "sales",
+  //   });
+  // };
+
+  return User;
+};
