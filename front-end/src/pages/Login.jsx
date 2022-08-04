cd66c864259a8ab96473449d1acb536e3a8d7616
Mauro Correia <mauroac1@outlook.com>
2022-08-04 16:50:46 -0300

    feat: validating email and password.

--- front-end/src/pages/Login.jsx
+++ front-end/src/pages/Login.jsx
@@ -3,10 +3,15 @@ import { useHistory } from 'react-router-dom';
 
 function Login() {
   const history = useHistory();
+
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+
   const minPasswordLength = 6;
-  const disabled = (password.length < minPasswordLength);
+  const errorMessage = false;
+  const emailRegex = /\S+@\S+\.\S+/;
+
+  const btnStatus = !((password.length >= minPasswordLength) && emailRegex.test(email));
 
   return (
     <section>
@@ -20,26 +25,26 @@ function Login() {
       <p> Password </p>
       <input
         data-testid="common_login__input-password"
-        type="text"
+        type="password"
         value={ password }
         onChange={ (e) => setPassword(e.target.value) }
       />
       <button
         data-testid="common_login__button-login"
         type="button"
-        disabled={ disabled }
+        disabled={ btnStatus }
       >
         LOGIN
       </button>
-
       <button
         data-testid="common_login__button-register"
         type="button"
         onClick={ () => history.push('/register') }
       >
         Ainda não tenho conta
-
       </button>
+      {errorMessage
+      && <p data-testid="common_login__element-invalid-email"> Mensagem de erro.</p>}
     </section>
   );
 }
