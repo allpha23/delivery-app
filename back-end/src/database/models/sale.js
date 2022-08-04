e820335169d9c6e1512be732b754c4ab92437904
brucealberto <brucealberto2@gmail.com>
2022-08-04 16:57:47 -0300

    feat: adiciona migração e entidade sale

---
+++ back-end/src/database/models/sale.js
@@ -0,0 +1,23 @@
+const Sale = (sequelize, DataTypes) => {
+  const Sale = sequelize.define(
+    "sale",
+    {
+      user_id: { type: DataTypes.INTEGER },
+      total_price: DataTypes.DECIMAL(9,2),
+      delivery_address: DataTypes.STRING,
+      delivery_number: DataTypes.STRING,
+      sale_date: { type: DataTypes.DATE, defaultValue: Date.now() },
+      status: DataTypes.STRING,
+      seller_id: DataTypes.INTEGER,
+    },
+    { timestamps: false }
+  );
+
+  Sale.associate = (models) => {
+    Sale.belongsTo(models.user, { as: "user", foreignKey: "user_id" });
+  };
+
+  return Sale;
+};
+
+module.exports = BlogPost;
