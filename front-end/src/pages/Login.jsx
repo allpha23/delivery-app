32be4d18121d50fa67770d037afef890faa662ff
Mauro Correia <mauroac1@outlook.com>
2022-08-04 16:10:43 -0300

    feat: login screen elements.

--- front-end/src/pages/Login.jsx
+++ front-end/src/pages/Login.jsx
@@ -1,8 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
   return (
-    <h1>Login</h1>
+    <>
+      <input type="text" value={ email } onChange={ (e) => setEmail(e.target.value) } />
+      <input
+        type="text"
+        value={ password }
+        onChange={ (e) => setPassword(e.target.value) }
+      />
+      <button type="button">LOGIN</button>
+      <button type="button">Ainda não tenho conta</button>
+    </>
   );
 }
 
