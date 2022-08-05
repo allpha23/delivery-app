c87a391e9b4250007671abdeadcd803cd59f43fb
allpha23 <allpha23@gmail.com>
2022-08-05 14:40:27 -0300

    feat(login): adiciona jwt token

--- back-end/src/services/loginService.js
+++ back-end/src/services/loginService.js
@@ -1,16 +1,22 @@
+const md5 = require('md5');
 const { user } = require('../database/models');
+const jwt = require('../utils/jwtGenerator');
 const AppError = require('../error/AppError');
 
 const login = async (email, password) => {
-  const users = await user.findAll({
-    where: {
-      email,
-      password,
-    },
-  });
+  const [users] = await user.findAll({ where: { email } });
 
-  if (!users.lenght) throw new AppError('User not found', 404);
-  return users;
+  if (!users) throw new AppError('User not found', 404);
+
+  const hash = md5(password);
+
+  if (hash !== users.password) throw new AppError('Invalid Password', 400);
+
+  delete users.password;
+
+  const token = jwt(users);
+  
+  return token;
 };
 
 module.exports = { login };
