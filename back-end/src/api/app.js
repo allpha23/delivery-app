aca4efc4a3a033345afa742c7f7b59b3d1a72347
Rafael Bonin <rafalyf11@gmail.com>
2022-08-08 14:42:23 -0300

    feat: requisicao post login base feita

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const errorMiddleware = require('../middlewares/error');
 const loginRoute = require('../routes/loginRoute');
+const cors = require('cors');
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 app.use('/login', loginRoute);
 
 app.use(errorMiddleware);
