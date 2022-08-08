dcb65e19819ed21781bf1dc5def6dc626fd54edb
brucealberto <brucealberto2@gmail.com>
2022-08-08 17:04:38 -0300

    feat:  product route

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -3,12 +3,14 @@ const cors = require('cors');
 const errorMiddleware = require('../middlewares/error');
 const loginRoute = require('../routes/loginRoute');
 const userRoute = require('../routes/userRoute');
+const productRoute = require('../routes/productRoute');
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 app.use('/login', loginRoute);
 app.use('/user', userRoute);
+app.use('/product', productRoute);
 
 app.use(errorMiddleware);
 
