54ba1c1cdd5debdf8995f1b0ae946cbb267c75c8
brucealberto <brucealberto2@gmail.com>
2022-08-09 15:31:33 -0300

    feat: productById route

--- back-end/src/routes/productRoute.js
+++ back-end/src/routes/productRoute.js
@@ -1,10 +1,11 @@
-const express = require('express');
-const productController = require('../controllers/productController');
+const express = require("express");
+const productController = require("../controllers/productController");
 // const validateUser = require('../middlewares/validateUser');
 
 const route = express.Router();
 
-route.post('/', productController.create);
-route.get('/', productController.getAll);
+route.post("/", productController.create);
+route.get("/", productController.getAll);
+route.get("/:id", productController.getById);
 
 module.exports = route;
