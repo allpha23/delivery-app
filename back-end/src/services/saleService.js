96663383cd7aeb97b9d0e1fea86c4866d590d08c
allpha23 <allpha23@gmail.com>
2022-08-10 19:22:37 -0300

    fix: sale service

--- back-end/src/services/saleService.js
+++ back-end/src/services/saleService.js
@@ -1,10 +1,14 @@
 const { sale } = require('../database/models');
+const { salesProduct } = require('../database/models');
 
 const create = async (data) => {
   const { userId, totalPrice, deliveryAddress, deliveryNumber, saleDate, status, sellerId } = data;
   const result = await sale.create(
     { userId, totalPrice, deliveryAddress, deliveryNumber, saleDate, status, sellerId },
-);
+  );
+
+  await salesProduct.create({ saleId: result.id, productId: 1, quantity: 1 });
+
   return result;
 };
 
