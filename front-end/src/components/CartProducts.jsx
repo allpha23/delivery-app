945a0be61e1cc361c5eae3438c0dd2ec11f19a6a
Rafael Bonin <rafalyf11@gmail.com>
2022-08-11 15:19:56 -0300

    feat: exportacao de arquivos

--- front-end/src/components/CartProducts.jsx
+++ front-end/src/components/CartProducts.jsx
@@ -1,10 +1,11 @@
-import React from "react";
+import React from 'react';
 
 function CartProducts() {
-
   return (
     <>
-      
+
     </>
   );
 }
+
+export default CartProducts;
--- front-end/src/pages/Checkout.jsx
+++ front-end/src/pages/Checkout.jsx
@@ -2,12 +2,9 @@ import React from 'react';
 import ClientNavegation from '../components/ClientNavegation';
 
 function Checkout() {
-
   return (
-    <>
-      <ClientNavegation />
-      
-      
-    </>
+    <ClientNavegation />
   );
 }
+
+export default Checkout;
