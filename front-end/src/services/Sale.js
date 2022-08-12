8ea5907551eb0e828f47ca990ac1381526727985
Lucas Barbosa <lukeb.trybe@gmail.com>
2022-08-12 18:50:50 -0300

    refactor: corrige endpoint na func getSalesByUserId

--- front-end/src/services/Sale.js
+++ front-end/src/services/Sale.js
@@ -1,8 +1,13 @@
 import axios from 'axios';
 
 const getSalesByUserId = async (id) => {
+  const sales = await axios.get(`http://localhost:3001/sale/user/${id}`);
+  return sales;
+};
+
+const getSaleById = async (id) => {
   const sales = await axios.get(`http://localhost:3001/sale/${id}`);
   return sales;
 };
 
-export default getSalesByUserId;
+export { getSalesByUserId, getSaleById };
