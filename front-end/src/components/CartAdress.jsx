2ba3b8fa502c0b2adb898ee982e4fc31d07ffc81
Rafael Bonin <rafalyf11@gmail.com>
2022-08-12 16:51:57 -0300

    feat: logica 20 e 21 adicionadas, falta validacao de token

--- front-end/src/components/CartAdress.jsx
+++ front-end/src/components/CartAdress.jsx
@@ -1,6 +1,31 @@
-import React from 'react';
+import React, { useState, useContext } from 'react';
+import { Redirect } from 'react-router-dom';
+import createSale from '../services/createSale';
+import Context from '../context/Context';
 
 function CartAdress() {
+  const { cart } = useContext(Context);
+
+  const [address, setAddress] = useState('a');
+  const [adrsNumber, setAdrsNumber] = useState(0);
+  const [redir, setRedir] = useState(false);
+  const [saleId, setSaleId] = useState();
+
+  const totalPrice = cart
+    .reduce((price, item) => (Number(item.price) * item.quantity) + price, 0)
+    .toFixed(2);
+
+  const createSalenRedirect = async () => {
+    try {
+      const sale = await createSale(Number(totalPrice), address, adrsNumber);
+      console.log(sale.data);
+      setSaleId(sale.data.id);
+      setRedir(true);
+    } catch (e) {
+      console.log(e.message);
+    }
+  };
+
   return (
     <form>
       <label htmlFor="seller">
@@ -16,6 +41,7 @@ function CartAdress() {
       <label htmlFor="address">
         Endereco
         <input
+          onChange={ (e) => setAddress(e.target.value) }
           type="text"
           id="address"
           name="address"
@@ -25,6 +51,7 @@ function CartAdress() {
       <label htmlFor="number">
         Numero
         <input
+          onChange={ (e) => setAdrsNumber(e.target.value) }
           type="number"
           name="number"
           data-testid="customer_checkout__input-addressNumber"
@@ -33,9 +60,13 @@ function CartAdress() {
       <button
         type="button"
         data-testid="customer_checkout__button-submit-order"
+        onClick={ createSalenRedirect }
       >
         FINALIZAR PEDIDO
       </button>
+
+      { redir && <Redirect to={ `/customer/orders/${saleId}` } />}
+
     </form>
   );
 }
