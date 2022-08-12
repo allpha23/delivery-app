16e3c4546be60c03aa70904fed5850fbda221e43
Rafael Bonin <rafalyf11@gmail.com>
2022-08-12 14:43:09 -0300

    fix: lint

--- front-end/src/pages/Products.jsx
+++ front-end/src/pages/Products.jsx
@@ -37,14 +37,14 @@ export default function Products() {
 
       <button
         type="button"
-        data-testid='customer_products__button-cart'
-        
+        data-testid="customer_products__button-cart"
         onClick={ () => setRedir(true) }
         disabled={ cart.length === 0 }
       >
         <p
-          data-testid="customer_products__checkout-bottom-value">
-            { priceCheck }
+          data-testid="customer_products__checkout-bottom-value"
+        >
+          { priceCheck }
         </p>
       </button>
       {redir && <Redirect to="/customer/checkout" />}
