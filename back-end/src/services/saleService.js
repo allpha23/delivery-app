36fd2aeb9a5f53eafdff000080ce0cd17fce666b
Lucas Barbosa <lukeb.trybe@gmail.com>
2022-08-12 17:46:08 -0300

    feat: add método getSaleById ao saleService

--- back-end/src/services/saleService.js
+++ back-end/src/services/saleService.js
@@ -1,9 +1,9 @@
 const { sale, salesProduct, user, product } = require('../database/models');
 
 const create = async (data) => {
-  const { userId, totalPrice, deliveryAddress, deliveryNumber, saleDate, sellerId } = data;
+  const { userId, totalPrice, deliveryAddress, deliveryNumber, sellerId } = data;
   const result = await sale.create(
-    { userId, totalPrice, deliveryAddress, deliveryNumber, saleDate, status: 'pendente', sellerId },
+    { userId, totalPrice, deliveryAddress, deliveryNumber, saleDate: Date.now(), status: 'pendente', sellerId },
   );
 
   await salesProduct.create({
@@ -15,7 +15,7 @@ const create = async (data) => {
   return result;
 };
 
-const getById = async (id) => {
+const getByUserId = async (id) => {
   const result = await sale.findAll({
     where: { userId: id },
     include: [
@@ -36,4 +36,15 @@ const update = async (data, id) => {
   return { message: 'update successfully' };
 };
 
-module.exports = { create, getById, getAll, update };
+const getSaleById = async (id) => {
+  const [result] = await sale.findAll({
+    where: { id },
+    include: [
+      { model: user, as: 'Seller', attributes: { exclude: ['password'] } },
+      { model: product, as: 'products', through: { attributes: { include: ['quantity'] } } },
+  ],
+  });
+  return result; 
+};
+
+module.exports = { create, getByUserId, getAll, update, getSaleById };
