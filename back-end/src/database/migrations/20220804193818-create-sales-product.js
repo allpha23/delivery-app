491037d7ab3a2c177c2a93326062db1e13486d5d
allpha23 <allpha23@gmail.com>
2022-08-14 02:43:42 -0300

    fix(model): table name camelcase to underscore

--- back-end/src/database/migrations/20220804193818-create-sales-product.js
+++ back-end/src/database/migrations/20220804193818-create-sales-product.js
@@ -1,7 +1,7 @@
 "use strict";
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable("salesProducts", {
+    await queryInterface.createTable("sales_products", {
       id: {
         allowNull: false,
         autoIncrement: true,
@@ -28,6 +28,6 @@ module.exports = {
     });
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable("salesProducts");
+    await queryInterface.dropTable("sales_products");
   },
 };
