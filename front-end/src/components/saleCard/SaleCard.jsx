73a5d2b1cfa69fbbbc430e299591d09c2d8bbd39
allpha23 <allpha23@gmail.com>
2022-08-17 13:16:05 -0300

    feat(SaleCard.css): change status color

--- front-end/src/components/saleCard/SaleCard.jsx
+++ front-end/src/components/saleCard/SaleCard.jsx
@@ -1,11 +1,23 @@
 import { PropTypes } from 'prop-types';
+import { useState, useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
 import formatDate from '../formatDate';
 import styles from './styles.module.scss';
 
 export default function SaleCard(props) {
   const { id, date, status, price } = props;
+  const [isPreper, setIsPreper] = useState(false);
+  const [isDelivered, setIsDelivered] = useState(false);
   const history = useHistory();
+
+  const handleStatus = () => {
+    if (status === 'Preparando') setIsPreper(true);
+    if (status === 'Entregue') setIsDelivered(true);
+  };
+
+  useEffect(() => {
+    handleStatus();
+  }, []);
 
   return (
     <div
@@ -22,7 +34,10 @@ export default function SaleCard(props) {
           {id}
         </p>
       </div>
-      <div className={ styles.status }>
+      <div
+        className={ `${styles.status}
+        ${isPreper && styles.preper} ${isDelivered && styles.delivered}` }
+      >
         <p
           data-testid={ `customer_orders__element-delivery-status-${id}` }
         >
